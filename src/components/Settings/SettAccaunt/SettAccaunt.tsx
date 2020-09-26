df6fa9d7b3bb2d2906287d076fc5e43f1fc005f5
boyallex <60341259+boyallex@users.noreply.github.com>
2020-09-27 00:13:20 +0300

    Work (30min)

---
+++ src/components/Settings/SettAccaunt/SettAccaunt.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from "react";
+import styles from "./styles.module.scss";
+
+type PropsT = {
+
+}
+
+export const Accaunt: FC<PropsT> = ({
+
+}) => {
+    return (
+        <div className={styles.wrapper}>
+
+
+            <div className={styles.tikTokAcc}>
+                <div>
+                    <img src="" alt="ava"/>
+
+                </div>
+            </div>
+
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
--- src/pages/Settings_m/Settings.tsx
+++ src/pages/Settings_m/Settings.tsx
@@ -4,19 +4,49 @@ import NavBar_m from "../../components/NavBar_m/NavBar_m";
 import TopNavbar from "../../components/TopNavbar/TopNavbar";
 import {useSelector} from "react-redux";
 import {RootStateType} from "../../redux/store";
+import {Card} from "../../components/Card/Card";
+import {Accaunt} from "../../components/Settings/SettAccaunt/SettAccaunt";
+import styles from "./styles.module.scss";
+
+/*
+* здесь нужно подавать в Card паддинги и разрмеры разные, в зависимости от isDesktop
+* Предоставляю сие развлечение тебе
+* Весь остальной адаптив уже сделан
+* */
 
 const Settings = () => {
-   const isDesktop = useSelector((state: RootStateType) => state.app.isDesktop)
+    const isDesktop = useSelector((state: RootStateType) => state.app.isDesktop)
 
-   return (
-      <Page bg={"#E5E5EA"}>
-         {!isDesktop && <TopNavbar label={"Настройки"} logo={true} br={"0px 0px 11px 11px"}/>}
-         <div>
-            {/* тут будет основной контейнер*/}
-         </div>
-         <NavBar_m pageName={"Settings"} newTasksNumber={3}/>
-      </Page>
-   )
+    return (
+        <Page bg={"#E5E5EA"}>
+            {!isDesktop && <TopNavbar label={"Настройки"} logo={true} br={"0px 0px 11px 11px"}/>}
+            <div>
+                <Card>
+                    <div className={styles.title}>
+                        Аккаунт
+                    </div>
+                    <Accaunt />
+                </Card>
+                <Card>
+                    <div className={styles.title}>
+                        Уведомления
+                    </div>
+                </Card>
+                <Card>
+                    <div className={styles.title}>
+                        Вывод средств
+                    </div>
+                </Card>
+                <Card>
+                    <div className={styles.title}>
+                        Поддержка
+                    </div>
+                </Card>
+
+            </div>
+            <NavBar_m pageName={"Settings"} newTasksNumber={3}/>
+        </Page>
+    )
 }
 
 export default Settings
