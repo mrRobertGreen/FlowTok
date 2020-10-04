1c4e8ca56dfbf2d7f30cf01007b1391871f86983
boyallex <60341259+boyallex@users.noreply.github.com>
2020-10-04 09:00:54 +0300

    Work(10min)

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import {Page} from "../../components/Page/Page";
+import NavBar_m from "../../components/NavBar_m/NavBar_m";
 
 export const Panel = () => {
    return (
       <Page bg={"#EFEFF4"}>
-
+      <NavBar_m newTasksNumber={3} pageName={"Profile"}/>
+      
       </Page>
    )
 }
