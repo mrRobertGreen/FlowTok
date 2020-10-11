0d19c6a6a0e0de98f079641eed4cddaec671b7f9
Robert Green <mrrobertgreen17@gmail.com>
2020-10-11 16:27:35 +0300

    work - 0.2h
    tooltip

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import styles from "./styles.module.scss";
 import container from "../../media/images_new/container.svg";
 import VerticalLine from "../../media/images_new/VerticalLine.svg";
@@ -18,16 +18,23 @@ type PropsT = {
 export const Container: FC<PropsT> = ({isInformed = false, data, buyData}) => {
 
    const {image, need, quantity, type} = data
+   const [isTooltip, setIsTooltip]  = useState(false)
 
+   const onOpenTooltip = () => {
+      setIsTooltip(true)
+   }
+   const onCloseTooltip = () => {
+      setIsTooltip(false)
+   }
 
-    return (
-        <div className={styles.wrapper}>
-            <Modal isOpen={isInformed}>
-                <ToolTip />
-            </Modal>
+   return (
+      <div className={styles.wrapper}>
+         <Modal isOpen={isTooltip}>
+            <ToolTip onClose={onCloseTooltip}/>
+         </Modal>
          <div className={styles.title}>
             Контейнер {type}
-            <InformedButton isInformed={isInformed}/>
+            <InformedButton isInformed={isInformed} onClick={onOpenTooltip}/>
             {buyData ? <p className={styles.text__little_2}>Цена: {buyData.cost} ₽ за шт.</p> : <div/>}
          </div>
          <div className={styles.container}>
@@ -46,20 +53,22 @@ export const Container: FC<PropsT> = ({isInformed = false, data, buyData}) => {
 
 type InfoProps = {
    isInformed: boolean
+   onClick: () => void,
 }
 
 
-const InformedButton: FC<InfoProps> = ({isInformed}) => {
+const InformedButton: FC<InfoProps> = ({isInformed, onClick}) => {
 
-    if (isInformed) {
-        return (
-            <img src={info} style={{paddingLeft: "9px", height: "19px"}} alt=""/>
-        )
-    } else {
-        return (
-            <span/>
-        )
-    }
+   if (isInformed) {
+      return (
+         <img src={info} style={{paddingLeft: "9px", height: "19px"}} alt="" onClick={onClick}/>
+      )
+   } else {
+      return (
+         <span/>
+      )
+   }
+}
 
 // const Cost: FC<InfoProps> = ({isInformed}) => {
 //     if (isInformed) {
