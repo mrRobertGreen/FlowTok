8a007c7c2344361fe01da22428770f6cf51893c7
boyallex <60341259+boyallex@users.noreply.github.com>
2020-10-12 17:46:45 +0300

    Work(35min)

--- src/pages/Landing/Description/Description.tsx
+++ src/pages/Landing/Description/Description.tsx
@@ -1,22 +1,17 @@
 import React, {FC} from "react";
 import styles from "./styles.module.scss";
+import {useTranslation} from "react-i18next";
 
 export const Description: FC = ({}) => {
+    const {t} = useTranslation();
+
     return (
         <div className={styles.wrapper}>
-            <p className={styles.description} style={{marginBottom: "38px"}}>Обеспечьте себе дополнительный доход,
-                выкупая доли или целые морские грузовые контейнеры.
+            <p className={styles.description} style={{marginBottom: "38px"}}>{t("description-first-text")}
             </p>
-            <p className={styles.description} style={{marginBottom: "14px"}}>Наша компания Take Container является
-                брокером в мире морских грузоперевозок.
-                Агенты нашей компании занимаются поиском, подбором, проверкой документов морских контейнеров.
-                И самое главное: заключение договор на владения морскими контейнерами разных типов, снимая все
-                юридические вопросы с наших клиентов.
+            <p className={styles.description} style={{marginBottom: "14px"}}>{t("description-second-text")}
             </p>
-            <p className={styles.description} style={{marginBottom: "76px"}}>Мировой рынок морских перевозок имеет
-                тенденцию роста,
-                особенно после пандемии, средне годовой рост данной отросли ровняется 3%.
-                Именно поэтому мы создали компанию по поиску, покупки и распределению на маршруты морских контейнеров.
+            <p className={styles.description} style={{marginBottom: "76px"}}>{t("description-third-text")}
             </p>
 
             <div>
