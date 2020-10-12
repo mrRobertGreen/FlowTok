0dad954de8a777ff45f0aa5301ee2b8961b9e72a
boyallex <60341259+boyallex@users.noreply.github.com>
2020-10-12 22:49:28 +0300

    Work(1h15min)

--- src/components/Profile_mPage/MainBlock/Refs/Refs.tsx
+++ src/components/Profile_mPage/MainBlock/Refs/Refs.tsx
@@ -14,74 +14,159 @@ import {useMedia} from "react-media";
 import {GLOBAL_MEDIA_QUERIES} from "../../../Page/Page";
 import separatorY from "../../../../media/icons/separator_y.svg"
 import {useTranslation} from "react-i18next";
+import info from "../../../../media/images_new/Info.svg";
+import Modal from "../../../common/Modal/Modal";
+import {ToolTip} from "../../../ToolTip/ToolTip";
+import men from "../../../../media/images_new/user copy.png";
 
 type PropsType = {
-   refData: ReferralT
+    refData: ReferralT
 }
 
 export const Refs: FC<PropsType> = ({refData}) => {
-   const [isCopied, setIsCopied] = useState(false)
+    const [isCopied, setIsCopied] = useState(false)
 
-   const queries = useMedia({queries: GLOBAL_MEDIA_QUERIES})
-   const {t} = useTranslation()
+    const queries = useMedia({queries: GLOBAL_MEDIA_QUERIES})
+    const {t} = useTranslation()
+    const [isTooltip, setIsTooltip] = useState(false)
 
-   const onCopy = async (text: string) => {
-      await navigator.clipboard.writeText(text)
-      console.log("click")
-      setIsCopied(true)
-      setTimeout(() => {
-         setIsCopied(false)
-      }, 3000)
-   }
+    const onOpenTooltip = () => {
+        setIsTooltip(true)
+    }
+    const onCloseTooltip = () => {
+        setIsTooltip(false)
+    }
 
-   const {link, money, referrals} = refData
+    const onCopy = async (text: string) => {
+        await navigator.clipboard.writeText(text)
+        console.log("click")
+        setIsCopied(true)
+        setTimeout(() => {
+            setIsCopied(false)
+        }, 3000)
+    }
 
-   return (
-      <div className={styles.wrapper}>
-         <div>
-            <div className={styles.title}>
-               {t("refs-title")}
+    const {link, money, referrals} = refData
+
+    return (
+        <div className={styles.wrapper}>
+            <Modal isOpen={isTooltip}>
+                <ToolTip onClose={onCloseTooltip} ref={"ref"}/>
+            </Modal>
+            <div>
+                <div className={styles.title}>
+                    {t("refs-title")}
+                    <InformedButton isInformed={true} onClick={onOpenTooltip}/>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.input}>
+                        <Input type="text" readOnly={true} value={link} mod={"grey"}/>
+                    </div>
+                    <div className={styles.btn}>
+                        <Button
+                            mod={"gradient"}
+                            onClick={() => onCopy(link)}
+                            disabled={isCopied}
+                            style={{background: isCopied ? "#00BA32" : ""}}
+                        >
+                            {t("refs-btn")}
+                        </Button>
+                    </div>
+                </div>
             </div>
-            <div className={styles.row}>
-               <div className={styles.input}>
-                  <Input type="text" readOnly={true} value={link} mod={"grey"}/>
-               </div>
-               <div className={styles.btn}>
-                  <Button
-                     mod={queries.largeTablet ? "gradient" : "copy"}
-                     onClick={() => onCopy(link)}
-                     disabled={isCopied}
-                     style={{background: isCopied ? "#00BA32" : ""}}
-                  >
-                     {queries.largeTablet && t("refs-btn")}
-                  </Button>
-               </div>
+            {!queries.largeTablet && <Separator m={"20px 0"}/>}
+            <div className={styles.statsBlock}>
+                <div className={styles.item}>
+                    <div className={styles.label}>
+                        {t("refs-count")}
+                    </div>
+                    <div className={styles.numbers}>
+                        <p>
+                            145
+                            <img src={men} className={styles.men} alt=""/>
+                        </p>
+                        {/*{(Object.keys(referrals)as Array<keyof typeof referrals>).map((key, idx) => (*/}
+                        {/*   <div key={idx}>*/}
+                        {/*      {referrals[key] !== 0 || key === "a" && <div>{idx + 1}: {referrals[key]}*/}
+                        {/*      }*/}
+                        {/*   </div>*/}
+                        {/*))}*/}
+                    </div>
+                </div>
+                {queries.largeTablet && <Separator m={"20px 0"}/>}
+                <div className={styles.item}>
+                    <div className={styles.label}>
+                        {t("refs-earned")}
+                    </div>
+                    <div className={styles.numbers}>
+                        {money}₽
+                    </div>
+                </div>
             </div>
-         </div>
-         {!queries.largeTablet && <Separator m={"20px 0"}/>}
-         <div className={styles.statsBlock}>
-            <div className={styles.item}>
-               <div className={styles.label}>
-                  {t("refs-count")}
-               </div>
-               <div className={styles.numbers}>
-                  {(Object.keys(referrals)as Array<keyof typeof referrals>).map((key, idx) => (
-                     <div key={idx}>
-                        {referrals[key] !== 0 || key === "a" && <div>{idx + 1}: {referrals[key]}</div>}
-                     </div>
-                  ))}
-               </div>
+            {!queries.largeTablet && <Separator m={"20px 7px"}/>}
+            <div className={styles.refPeople}>
+                <div className={styles.refPeople__container}>
+                    <div className={styles.box}>
+                        <p className={styles.refPeople__level}>1</p>
+                        <div className={styles.numbers_container}>
+                            <p className={styles.refPeople__numbers}>6</p>
+                            <img src={men} className={styles.men} alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.separator}></div>
+                    <div className={styles.box}>
+                        <p className={styles.refPeople__level}>1</p>
+                        <div className={styles.numbers_container}>
+                            <p className={styles.refPeople__numbers}>6</p>
+                            <img src={men} className={styles.men} alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.separator}></div>
+                    <div className={styles.box}>
+                        <p className={styles.refPeople__level}>1</p>
+                        <div className={styles.numbers_container}>
+                            <p className={styles.refPeople__numbers}>6</p>
+                            <img src={men} className={styles.men} alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.separator}></div>
+                    <div className={styles.box}>
+                        <p className={styles.refPeople__level}>1</p>
+                        <div className={styles.numbers_container}>
+                            <p className={styles.refPeople__numbers}>6</p>
+                            <img src={men} className={styles.men} alt=""/>
+                        </div>
+                    </div>
+                    <div className={styles.separator}></div>
+                    <div className={styles.box}>
+                        <p className={styles.refPeople__level}>1</p>
+                        <div className={styles.numbers_container}>
+                            <p className={styles.refPeople__numbers}>6</p>
+                            <img src={men} className={styles.men} alt=""/>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {queries.largeTablet && <Separator m={"20px 0"}/>}
-            <div className={styles.item}>
-               <div className={styles.label}>
-                  {t("refs-earned")}
-               </div>
-               <div className={styles.numbers}>
-                  {money}₽
-               </div>
-            </div>
-         </div>
-      </div>
-   )
+        </div>
+    )
+}
+
+type InfoProps = {
+
+    isInformed: boolean
+    onClick: () => void,
+}
+
+const InformedButton: FC<InfoProps> = ({isInformed, onClick}) => {
+
+    if (isInformed) {
+        return (
+            <img src={info} style={{paddingLeft: "9px", height: "19px"}} alt="" onClick={onClick}/>
+        )
+    } else {
+        return (
+            <span/>
+        )
+    }
+
 }
--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -5,71 +5,92 @@ import {ContainerT} from "../../redux/user/user-reducer";
 import {useParams} from "react-router";
 
 type PropsT = {
-   onClose: () => void
+    onClose: () => void,
+    ref?: string
 }
 
 
-export const ToolTip: FC<PropsT> = ({onClose}) => {
+export const ToolTip: FC<PropsT> = ({onClose, ref}) => {
 
-   // @ts-ignore
-   const {type} = useParams() as ContainerT;
+    // @ts-ignore
+    const {type} = useParams() as ContainerT;
 
-   switch (type) {
-      case "small":
-         return (
+    if (ref !== undefined) {
+        return (
             <div className={styles.wrapper}>
-               <div className={styles.headerContainer}>
-                  <p className={styles.header}>Контейнер Small TEU</p>
-                  <button className={styles.crossBtn} onClick={onClose}><img src={cross} style={{width: "12px"}}
-                                                                             alt=""/></button>
-               </div>
-               <p className={styles.text}>Высота: 2591 см</p>
-               <p className={styles.text}>Длина: 6059 см</p>
-               <p className={styles.text}>Ширина : 2438 см</p>
-               <p className={styles.text}>Высота проема: 2260 см</p>
-               <p className={styles.text}>Ширина проема: 2339 см</p>
+                <div className={styles.headerContainer}>
+                    <p className={styles.header}>Контейнер Small TEU</p>
+                    <button className={styles.crossBtn} onClick={onClose}><img src={cross}
+                                                                               style={{width: "12px"}}
+                                                                               alt=""/></button>
+                </div>
+                <p className={styles.text}>Высота: 2591 см</p>
+                <p className={styles.text}>Длина: 6059 см</p>
+                <p className={styles.text}>Ширина : 2438 см</p>
+                <p className={styles.text}>Высота проема: 2260 см</p>
+                <p className={styles.text}>Ширина проема: 2339 см</p>
             </div>
-         )
-      case "large":
-         return (
-            <div className={styles.wrapper}>
-               <div className={styles.headerContainer}>
-                  <p className={styles.header}>Контейнер Large FEU</p>
-                  <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
-               </div>
-               <p className={styles.text}>Высота: 2591 см</p>
-               <p className={styles.text}>Длина: 12192</p>
-               <p className={styles.text}>Ширина : 2438 см</p>
-               <p className={styles.text}>Высота проема: 2260 см</p>
-               <p className={styles.text}>Ширина проема: 2339 см</p>
-            </div>
-         )
-      case "refrigerator":
-         return (
-            <div className={styles.wrapper}>
-               <div className={styles.headerContainer}>
-                  <p className={styles.header}>Контейнер Refrigerator Carrier</p>
-                  <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
-               </div>
-               <p className={styles.text}>Компрессор: бессальниковый полугерметичный
-                  6-ти цилиндровый</p>
-               <p className={styles.text}>Высота: 2591 см</p>
-               <p className={styles.text}>Длина: 12192 см</p>
-               <p className={styles.text}>Ширина : 2438 см</p>
-            </div>
-         )
-      default:
-         return (
-             <div className={styles.wrapper}>
-                 <div className={styles.headerContainer}>
-                     <p className={styles.header}>Оффшорный счет</p>
-                     <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
-                 </div>
-                 <p className={styles.text}>Ваш личный счёт для возможности выполнения операций по счёту.
-                     Так как деньги поступают от компаний расположенных в разных странах мира,
-                     Вам был создан счёт в партнерском банке для приёма платежей.
-                     Для вывода средств переведите на Ваш личный баланс и выберете удобный способ выплаты.</p>
-             </div>
-         )
-   }
+        )
+    } else {
+
+        switch (type) {
+            case "small":
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.headerContainer}>
+                            <p className={styles.header}>Контейнер Small TEU</p>
+                            <button className={styles.crossBtn} onClick={onClose}><img src={cross}
+                                                                                       style={{width: "12px"}}
+                                                                                       alt=""/></button>
+                        </div>
+                        <p className={styles.text}>Высота: 2591 см</p>
+                        <p className={styles.text}>Длина: 6059 см</p>
+                        <p className={styles.text}>Ширина : 2438 см</p>
+                        <p className={styles.text}>Высота проема: 2260 см</p>
+                        <p className={styles.text}>Ширина проема: 2339 см</p>
+                    </div>
+                )
+            case "large":
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.headerContainer}>
+                            <p className={styles.header}>Контейнер Large FEU</p>
+                            <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
+                        </div>
+                        <p className={styles.text}>Высота: 2591 см</p>
+                        <p className={styles.text}>Длина: 12192</p>
+                        <p className={styles.text}>Ширина : 2438 см</p>
+                        <p className={styles.text}>Высота проема: 2260 см</p>
+                        <p className={styles.text}>Ширина проема: 2339 см</p>
+                    </div>
+                )
+            case "refrigerator":
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.headerContainer}>
+                            <p className={styles.header}>Контейнер Refrigerator Carrier</p>
+                            <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
+                        </div>
+                        <p className={styles.text}>Компрессор: бессальниковый полугерметичный
+                            6-ти цилиндровый</p>
+                        <p className={styles.text}>Высота: 2591 см</p>
+                        <p className={styles.text}>Длина: 12192 см</p>
+                        <p className={styles.text}>Ширина : 2438 см</p>
+                    </div>
+                )
+            default:
+                return (
+                    <div className={styles.wrapper}>
+                        <div className={styles.headerContainer}>
+                            <p className={styles.header}>Оффшорный счет</p>
+                            <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
+                        </div>
+                        <p className={styles.text}>Ваш личный счёт для возможности выполнения операций по счёту.
+                            Так как деньги поступают от компаний расположенных в разных странах мира,
+                            Вам был создан счёт в партнерском банке для приёма платежей.
+                            Для вывода средств переведите на Ваш личный баланс и выберете удобный способ выплаты.</p>
+                    </div>
+                )
+        }
+    }
 }
