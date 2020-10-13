2fdc170eccb18e7ede6ffc1354d826164638197b
Robert Green <mrrobertgreen17@gmail.com>
2020-10-13 14:53:42 +0300

    work - 0.7h
    TicketForm

--- src/pages/Support/Support.tsx
+++ src/pages/Support/Support.tsx
@@ -2,6 +2,8 @@ import React, {FC} from "react";
 import TopNavbar from "../../components/TopNavbar/TopNavbar";
 import {useTranslation} from "react-i18next";
 import {Page} from "../../components/Page/Page";
+import Modal from "../../components/common/Modal/Modal";
+import {TicketForm} from "../../components/Support/TicketForm/TicketForm";
 
 export const Support: FC = () => {
     const {t} = useTranslation();
@@ -9,7 +11,7 @@ export const Support: FC = () => {
     return (
         <Page>
             <TopNavbar label={t("support-title")} subLabel={t("support-subTitle")}/>
-
+      
         </Page>
 
     )
