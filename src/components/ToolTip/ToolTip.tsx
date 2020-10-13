d2e0b1417a8a2d2ee0d6c00f306de950c0deb7f6
boyallex <60341259+boyallex@users.noreply.github.com>
2020-10-13 12:35:59 +0300

    Work(0.75h)

--- src/components/ToolTip/ToolTip.tsx
+++ src/components/ToolTip/ToolTip.tsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.scss";
 import cross from "../../media/images_new/Cross.svg";
 import {ContainerT} from "../../redux/user/user-reducer";
 import {useParams} from "react-router";
+import {useTranslation} from "react-i18next";
 
 type PropsT = {
     onClose: () => void,
@@ -14,21 +15,27 @@ export const ToolTip: FC<PropsT> = ({onClose, isRef}) => {
 
     // @ts-ignore
     const {type} = useParams() as ContainerT;
+    const {t} = useTranslation();
 
     if (isRef) {
         return (
             <div className={styles.wrapper}>
                 <div className={styles.headerContainer}>
-                    <p className={styles.header}>Контейнер Small TEU</p>
+                    <p className={styles.header}>{t("referral-title")}</p>
                     <button className={styles.crossBtn} onClick={onClose}><img src={cross}
                                                                                style={{width: "12px"}}
                                                                                alt=""/></button>
                 </div>
-                <p className={styles.text}>Высота: 2591 см</p>
-                <p className={styles.text}>Длина: 6059 см</p>
-                <p className={styles.text}>Ширина : 2438 см</p>
-                <p className={styles.text}>Высота проема: 2260 см</p>
-                <p className={styles.text}>Ширина проема: 2339 см</p>
+                <p className={styles.text}>{t("ref-1-text")}</p>
+                <p className={styles.text}>{t("ref-2-text")}</p>
+                <p className={styles.text}>{t("ref-3-text")}</p>
+                <p className={styles.text}>{t("ref-4-text")}</p>
+                <p className={styles.percent_header}>{t("percent-title")}</p>
+                <p className={styles.text}>{t("level-1-text")}</p>
+                <p className={styles.text}>{t("level-2-text")}</p>
+                <p className={styles.text}>{t("level-3-text")}</p>
+                <p className={styles.text}>{t("level-4-text")}</p>
+                <p className={styles.text}>{t("level-5-text")}</p>
             </div>
         )
     } else {
@@ -38,57 +45,53 @@ export const ToolTip: FC<PropsT> = ({onClose, isRef}) => {
                 return (
                     <div className={styles.wrapper}>
                         <div className={styles.headerContainer}>
-                            <p className={styles.header}>Контейнер Small TEU</p>
+                            <p className={styles.header}>{t("container-small-title")}</p>
                             <button className={styles.crossBtn} onClick={onClose}><img src={cross}
                                                                                        style={{width: "12px"}}
                                                                                        alt=""/></button>
                         </div>
-                        <p className={styles.text}>Высота: 2591 см</p>
-                        <p className={styles.text}>Длина: 6059 см</p>
-                        <p className={styles.text}>Ширина : 2438 см</p>
-                        <p className={styles.text}>Высота проема: 2260 см</p>
-                        <p className={styles.text}>Ширина проема: 2339 см</p>
+                        <p className={styles.text}>{t("small-height-text")}</p>
+                        <p className={styles.text}>{t("small-length-text")}</p>
+                        <p className={styles.text}>{t("small-width-text")}</p>
+                        <p className={styles.text}>{t("small-opening-height-text")}</p>
+                        <p className={styles.text}>{t("small-opening-height-text")}</p>
                     </div>
                 )
             case "large":
                 return (
                     <div className={styles.wrapper}>
                         <div className={styles.headerContainer}>
-                            <p className={styles.header}>Контейнер Large FEU</p>
+                            <p className={styles.header}>{t("container-large-title")}</p>
                             <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
                         </div>
-                        <p className={styles.text}>Высота: 2591 см</p>
-                        <p className={styles.text}>Длина: 12192</p>
-                        <p className={styles.text}>Ширина : 2438 см</p>
-                        <p className={styles.text}>Высота проема: 2260 см</p>
-                        <p className={styles.text}>Ширина проема: 2339 см</p>
+                        <p className={styles.text}>{t("large-height-text")}</p>
+                        <p className={styles.text}>{t("large-length-text")}</p>
+                        <p className={styles.text}>{t("large-width-text")}</p>
+                        <p className={styles.text}>{t("large-opening-height-text")}</p>
+                        <p className={styles.text}>{t("large-opening-height-text")}</p>
                     </div>
                 )
             case "refrigerator":
                 return (
                     <div className={styles.wrapper}>
                         <div className={styles.headerContainer}>
-                            <p className={styles.header}>Контейнер Refrigerator Carrier</p>
+                            <p className={styles.header}>{t("refrigerator-title")}</p>
                             <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
                         </div>
-                        <p className={styles.text}>Компрессор: бессальниковый полугерметичный
-                            6-ти цилиндровый</p>
-                        <p className={styles.text}>Высота: 2591 см</p>
-                        <p className={styles.text}>Длина: 12192 см</p>
-                        <p className={styles.text}>Ширина : 2438 см</p>
+                        <p className={styles.text}>{t("compressor-text")}</p>
+                        <p className={styles.text}>{t("large-height-text")}</p>
+                        <p className={styles.text}>{t("large-length-text")}</p>
+                        <p className={styles.text}>{t("large-width-text")}</p>
                     </div>
                 )
             default:
                 return (
                     <div className={styles.wrapper}>
                         <div className={styles.headerContainer}>
-                            <p className={styles.header}>Оффшорный счет</p>
+                            <p className={styles.header}>{t("offshore-account-title")}</p>
                             <button className={styles.crossBtn} onClick={onClose}><img src={cross} alt=""/></button>
                         </div>
-                        <p className={styles.text}>Ваш личный счёт для возможности выполнения операций по счёту.
-                            Так как деньги поступают от компаний расположенных в разных странах мира,
-                            Вам был создан счёт в партнерском банке для приёма платежей.
-                            Для вывода средств переведите на Ваш личный баланс и выберете удобный способ выплаты.</p>
+                        <p className={styles.text}>{t("your-bill-text")}</p>
                     </div>
                 )
         }
